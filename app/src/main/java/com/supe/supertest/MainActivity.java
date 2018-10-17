78615017396c90c64118dc2e065b06ae7f914c6f
spuermax <13783141492@163.com>
2018-10-17 11:31:24 +0800

    Activity Test

--- app/src/main/java/com/supe/supertest/MainActivity.java
+++ app/src/main/java/com/supe/supertest/MainActivity.java
@@ -3,20 +3,44 @@ package com.supe.supertest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.supermax.base.common.aspect.ThreadAspect;
 import com.supermax.base.common.aspect.ThreadPoint;
 import com.supermax.base.common.aspect.ThreadType;
+import com.supermax.base.common.log.L;
 import com.supermax.base.mvp.QsActivity;
 
 public class MainActivity extends QsActivity {
 
-    @ThreadPoint(ThreadType.HTTP)
-    private void init1(){
+    @Override
+    public void initData(Bundle bundle) {
+        init1();
+        init2();
+        init3();
+        init4();
     }
 
     @Override
-    public void initData(Bundle bundle) {
-
+    public int layoutId() {
+        return R.layout.activity_main;
     }
 
+    @ThreadPoint(ThreadType.HTTP)
+    public void init1() {
+        L.i(initTag(), "thread ==" + Thread.currentThread().getName() + " is == " + Thread.currentThread().getId());
+    }
 
+    @ThreadPoint(ThreadType.WORK)
+    public void init2() {
+        L.i(initTag(), "thread ==" + Thread.currentThread().getName() + " is == " + Thread.currentThread().getId());
+    }
+
+    @ThreadPoint(ThreadType.SINGLE_WORK)
+    public void init3() {
+        L.i(initTag(), "thread ==" + Thread.currentThread().getName() + " is == " + Thread.currentThread().getId());
+    }
+
+    @ThreadPoint(ThreadType.MAIN)
+    public void init4() {
+        L.i(initTag(), "thread ==" + Thread.currentThread().getName() + " is == " + Thread.currentThread().getId());
+    }
 }
---
+++ app/src/main/java/com/supe/supertest/SuperApplication.java
@@ -0,0 +1,18 @@
+package com.supe.supertest;
+
+import com.supermax.base.QsApplication;
+
+/*
+ * @Author yinzh
+ * @Date   2018/10/17 10:43
+ * @Description
+ */
+public class SuperApplication extends QsApplication{
+
+
+    @Override
+    public boolean isLogOpen() {
+        return true;
+    }
+
+}
